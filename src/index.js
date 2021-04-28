6a607362f9c8553914567120c2cc53f3a8e82e78
phantom-5 <rickrudra@gmail.com>
2021-04-28 09:46:01 +0530

    no internet warning

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Swal from 'sweetalert2'
 
 window.addEventListener('orientationchange', ()=> {
   window.location.reload()
 })
+
+window.onoffline = async() => {
+  var swal_notif = await Swal.fire({
+    icon: 'error',
+    title: 'No Internet!',
+    text: 'Please check your internet connection.',
+    showConfirmButton: true,
+    confirmButtonText: 'Okay',
+    confirmButtonColor: 'red'
+  })
+  if(swal_notif.isConfirmed || swal_notif.isDismissed){
+  window.location.reload()
+  }
+}
 
 var count = Math.floor((Math.random() * 13) + 1);
 document.body.style.backgroundImage = "url('assets/landscape/"+count+".jpg')"
